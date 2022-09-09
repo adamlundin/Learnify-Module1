01bbe79c48aaca468355ed8c92b9626ad7d88e6d
Adam <92366358+adamlundin@users.noreply.github.com>
2022-09-09 20:08:19 +0200

    Part 11

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import './App.css';
+import './sass/main.scss';
 import Homepage from './pages/Homepage';
 import LoginPage from './pages/Login';
 import DetailPage from './pages/DetailPage';
+import Navigation from './components/Navigation';
 
 function App() {
-  return <>
-    <Switch>
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/login" component={LoginPage} />
-      <Route exact path="/detail" component={DetailPage} />
-    </Switch>
-  </>;
+  return (
+    <>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/detail" component={DetailPage} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
