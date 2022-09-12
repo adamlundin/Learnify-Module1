2841490dcd02b05747ad3a6bc96c98fc25231c1e
Adam <92366358+adamlundin@users.noreply.github.com>
2022-09-12 16:16:26 +0200

    Part 12

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import * as FaIcons from "react-icons/fa";
 import Logo from "../assets/logo.png";
 
 const Navigation = () => {
+const [sidebar, setSidebar] = useState(false)
+
+const showSidebar = () => setSidebar(!sidebar);
+
     return (
         <div className="nav-container">
             <div className="nav">
                 <div className="nav__left">
+                    <div className="nav__left__hamburger">
+                        <FaIcons.FaBars onClick={showSidebar}/>
+                        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                            <ul className="nav-menu-items" onClick={showSidebar}>
+                                <li className="cancel">
+                                    <FaIcons.FaChevronLeft />
+                                </li>
+                                <li className="nav-menu-items__header">Navigation</li>
+                                <li>Categories</li>
+                                <li>My Courses</li>
+                            </ul>
+                        </nav>
+                    </div>
                     <img className="nav__left__logo" src={Logo} alt="Logo"/>
                     <ul className="nav__left__list">
                         <div className="nav__left__list__item">Categories</div>
@@ -21,7 +38,7 @@ const Navigation = () => {
                             placeholder="Search Courses..."
                         />
                         <button className="nav__right__search__button">
-                            <FaIcons.FaSearch />
+                            <i className="fas fa-search"></i> 
                         </button>
                     </form>
                 </div>
