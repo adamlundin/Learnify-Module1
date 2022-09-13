c959f6c1a4dfb3f357673158dc9c66457691d409
Adam <92366358+adamlundin@users.noreply.github.com>
2022-09-13 13:37:12 +0200

    Part 6

--- Infrastructure/StoreContextSeed.cs
+++ Infrastructure/StoreContextSeed.cs
@@ -14,6 +14,19 @@ namespace Infrastructure
         public static async Task SeedAsync(StoreContext context, ILogger logger)
         {
             try {
+                if (!context.Categories.Any())
+                {
+                    var categoryData = File.ReadAllText("../Infrastructure/SeedData/categories.json");
+                    var categories = JsonSerializer.Deserialize<List<Category>>(categoryData);
+
+                    foreach (var item in categories)
+                    {
+                        context.Categories.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
                 if (!context.Courses.Any())
                 {
                     var courseData = File.ReadAllText("../Infrastructure/SeedData/courses.json");
@@ -22,6 +35,32 @@ namespace Infrastructure
                     foreach (var item in courses)
                     {
                         context.Courses.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Learnings.Any())
+                {
+                    var learningData = File.ReadAllText("../Infrastructure/SeedData/learnings.json");
+                    var learnings = JsonSerializer.Deserialize<List<Learning>>(learningData);
+
+                    foreach (var item in learnings)
+                    {
+                        context.Learnings.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Requirements.Any())
+                {
+                    var requirementData = File.ReadAllText("../Infrastructure/SeedData/requirements.json");
+                    var requirements = JsonSerializer.Deserialize<List<Requirement>>(requirementData);
+
+                    foreach (var item in requirements)
+                    {
+                        context.Requirements.Add(item);
                     }
 
                     await context.SaveChangesAsync();
