413d4f1ef2c6889b487e6e41d221ee7a430fea49
Adam <92366358+adamlundin@users.noreply.github.com>
2022-09-13 17:55:42 +0200

    Part 10

--- API/Dto/CourseDto.cs
+++ API/Dto/CourseDto.cs
@@ -33,6 +33,6 @@ namespace API.Dto
 
         public string Category { get; set; }
 
-        public DateTime LastUpdated { get; set; } = DateTime.Now;
+        public DateTime LastUpdated { get; set; }
     }
 }
