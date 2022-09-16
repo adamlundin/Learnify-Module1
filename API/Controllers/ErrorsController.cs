15a6fdbadedb21dd65a9f7508d11c4d8af96977c
Adam <92366358+adamlundin@users.noreply.github.com>
2022-09-16 10:52:06 +0200

    Part 1

---
+++ API/Controllers/ErrorsController.cs
@@ -0,0 +1,44 @@
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ErrorsController : BaseController
+    {
+        private readonly StoreContext _context;
+        public ErrorsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notFound")]
+        public ActionResult NotFoundMethod()
+        {
+            var category = _context.Categories.Find(42);
+
+            if(category == null) return NotFound();
+
+            return Ok();
+        }
+
+        [HttpGet("serverError")]
+        public ActionResult ServerErrorMethod()
+        {
+            var category = _context.Categories.Find(42);
+
+            return Ok(category.ToString());
+        }
+
+        [HttpGet("badRequest")]
+        public ActionResult BadRequestMethod()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("badRequest/{id}")]
+        public ActionResult BadIdMethod(int id)
+        {
+            return Ok();
+        }
+    }
+}
