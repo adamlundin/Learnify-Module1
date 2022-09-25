9c5b6aafd39c160565f04c22cfc7859ba8ef4947
Adam <92366358+adamlundin@users.noreply.github.com>
2022-09-25 15:23:39 +0200

    Part 13 - Adding Search Filters

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,9 +1,17 @@
 import React, {useEffect} from "react";
 import { Course } from "../models/course";
 import ShowCourses from "../components/ShowCourses";
-import { Row } from "antd";
+import { Card, Col, Radio, Row } from "antd";
 import { useAppDispatch, useAppSelector } from "../redux/store/configureStore";
 import { coursesSelector, getCoursesAsync } from "../redux/slice/courseSlice";
+import { categoriesSelector, getCategoriesAsync } from "../redux/slice/categorySlice";
+import { Category } from "../models/category";
+
+const sortOptions = [
+    {value: "title", label: "Alphabetical"},
+    {value: "priceDescending", label: "Price - High to low"},
+    {value: "priceAscending", label: "Price - Low to high"},
+]
 
 const Homepage = () => {
     const courses = useAppSelector(coursesSelector.selectAll);
@@ -14,6 +22,16 @@ const Homepage = () => {
         if (!coursesLoaded) dispatch(getCoursesAsync())
     }, [coursesLoaded, dispatch]);
 
+    const categories = useAppSelector(categoriesSelector.selectAll);
+
+    const getCategories = () => {
+        const catArray: any[] = [];
+        categories.forEach((category: Category) => {
+            catArray.push({value: category.id, label: category.name});
+        });
+        return catArray;
+    };
+
     return (
         <div className="course">
             <div className="course__header">
@@ -21,9 +39,21 @@ const Homepage = () => {
                 <h2>New Courses picked just for you...</h2>
             </div>
             <Row gutter={[24, 32]}>
-                {courses && courses.map((course: Course, index: number) => {
-                    return <ShowCourses key={index} course={course}/>
-                })}
+                <Col span={4}>
+                    <Card title="Sorting Options">
+                        <Radio.Group options={sortOptions}/>
+                    </Card>
+                    <Card title="Choose Category">
+                        <Radio.Group options={getCategories()}/>
+                    </Card>
+                </Col>
+                <Col span={20}>
+                    <Row gutter={[24, 32]}>
+                        {courses && courses.map((course: Course, index: number) => {
+                            return <ShowCourses key={index} course={course}/>
+                        })}
+                    </Row>
+                </Col>
             </Row>
         </div>
     );
