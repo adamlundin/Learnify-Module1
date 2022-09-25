f0575d1e428e9e5f5473f0dc9b2c3560611c5883
Adam <92366358+adamlundin@users.noreply.github.com>
2022-09-25 13:44:29 +0200

    Part 11 - Fetching Singel Product

--- client/src/redux/slice/courseSlice.ts
+++ client/src/redux/slice/courseSlice.ts
@@ -17,6 +17,17 @@ export const getCoursesAsync = createAsyncThunk<PaginatedCourse | undefined, voi
     }
 );
 
+export const getCourseAsync = createAsyncThunk<Course | undefined, { courseId: string }>(
+    "course/getCourseAsync",
+    async ({courseId}) => {
+        try {
+            return await agent.Courses.getById(courseId);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+);
+
 export const courseSlice = createSlice({
     name: "course",
     initialState: coursesAdapter.getInitialState({
@@ -31,8 +42,19 @@ export const courseSlice = createSlice({
         builder.addCase(getCoursesAsync.fulfilled, (state, action) => {
             coursesAdapter.setMany(state, action.payload!.data);
             state.status = "idle";
+            state.coursesLoaded = true;
         });
         builder.addCase(getCoursesAsync.rejected, (state) => {
+            state.status = "idle";
+        });
+        builder.addCase(getCourseAsync.pending, (state) => {
+            state.status = "pending";
+        });
+        builder.addCase(getCourseAsync.fulfilled, (state, action) => {
+            coursesAdapter.upsertOne(state, action.payload!);
+            state.status = "idle";
+        });
+        builder.addCase(getCourseAsync.rejected, (state) => {
             state.status = "idle";
         });
     },
