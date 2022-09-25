f4ff47bd4cb86062dc9928db7158fcbef7e58943
Adam <92366358+adamlundin@users.noreply.github.com>
2022-09-25 10:50:59 +0200

    Part 2 - Creating Action in Redux

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { LoginState } from "../redux/loginReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, LoginState } from "../redux/loginReducer";
 
 const LoginPage = () => {
+    const {visits} = useSelector((state: LoginState) => state);
 
-    const {visits} = useSelector((state: LoginState) => state)
+    const dispatch = useDispatch();
 
-    return <h1>Number of Visits: {visits}</h1>;
+    return (
+        <>
+            <h1>Number of Visits: {visits}</h1>
+            <button onClick={() => dispatch(increment(5))}>Increment</button>
+        </>
+    );
 }
 
 export default LoginPage;
