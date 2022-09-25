ef143cc7b4d60d3248fc506b519b9d5fefaf4155
Adam <92366358+adamlundin@users.noreply.github.com>
2022-09-25 11:21:26 +0200

    Part 3 - Creating Basket Slice

--- client/src/redux/store/configureStore.ts
+++ client/src/redux/store/configureStore.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { BasketSlice } from "../slice/basketSlice";
 import { loginSlice } from "../slice/loginSlice";
 
 export const store = configureStore({
     reducer: {
         login: loginSlice.reducer,
+        basket: BasketSlice.reducer,
     },
 });
 
